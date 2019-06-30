040b4f087cc774ca15dc5527e5e32fb67b962e0d
MasterPlow <haerphi@gmail.com>
2019-06-30 20:57:15 +0200

    ajout exercices

---
+++ scriptMemento.js
@@ -0,0 +1,72 @@
+(() => {
+    // ~~ PARTIE 1 ~~
+    // déclaration de variable
+
+    // déclara de constante
+
+    // déclaration d'un tableau de fruits : pomme, banane, poire
+
+    // avec une alerte affiche le 2eme element du tableaus (banane)
+
+    // avec un boucle FOR affiche tout les élément dans la console
+    console.log("Tableau fruit avec boucle FOR : ");
+        /* votre boucle ici */
+    
+    // avec un boucle FOREACH affiche tout les élément dans la console
+        console.log("Tableau fruit avec boucle FOREACH : ");
+        /* votre boucle ici */
+    
+    // demander à l'utilisateur d'entrer son age (avec prompt) et affichez le dans la console ou via une alert
+    
+    // écrit une condition qui vérfie si l'âge de l'utilisateur est plus grand que 18 (n'oubies pas le parseFloat)
+        // si c'est le cas affiche +18
+        // si c'est moins que 18 affiche -18
+        // si c'est égale à 18 affiche =18
+
+    // avec une boucle WHILE demandes lui s'il râle et tant qu'il ne dit pas "oui", re-demande lui. (tu peux aussi te rensigner sur la boucle do...while)
+
+    // Les tableaux
+    // à la liste(tableau) de fruits rajoute le fruit "kiwi"
+
+    // à la liste(tableau) de fruits supprime le fruit "banane"
+
+    // à la liste(tableau) de fruits trouve l'index de "poire"
+
+    // à la liste(tableau) de fruits regarde s'il y a "abrico"
+
+    // ~~ PARTIE 2 ~~
+    // 1.1 Récuperes le text "J'aime les cacaouette" dans via son ID - document.getElementById("id").innerText
+
+    // 1.2 Modifie la phrase juste avant par autre chose va innerText ou innerHtml ou textContent
+
+    // 2 Rajoute l'évement click au bonton adéquat et affiche une alerte lorsque l'on click dessus !
+
+    // 3.1 au clic sur le bouton, récupere le contenu de l'input
+
+    //3.2 modifier le tagert ci-dessous pour qu'il affiche le nombre de caractère que possède l'input
+        //(le changement doit se faire en temps réel et non juste quand on clic sur le bouton)
+
+    // ~~ BONUS ~~
+    // rajoutes un bouton dans l'html et au clic créer un genre de dark theme et light theme !
+    
+    // ~~ PARTIE 3 ~~
+    // 1.1 Réecrit dans le code la fonction <code>getRandomInt</code>
+
+    // 1.2 appele là une fois avec un min de 1 et un max de 3 et une autre fois avec un min de 0 et un max de 101
+    //affiche le résultat dans la console sous ce format : "Random min à max : x" (en remplace min par le minimum, max par le maximum et x par le nombre aléatoire).
+
+    // 2 Créer une fonction pour calculer le périmètre d'un carré, et son aire
+
+    // ~~ PARTIE 4 ~~
+    // 1.1 créer un objet nommé chat avec les propriétés nom et couleur.
+
+    // 1.2 affiche dans la console le nom du chat et sa couleur sous cette forme "Mon chat NOM est de couleur COULEUR" (remplaces NOM par le nom du chat et COULEUR par sa couleur)
+
+    // 2.1 créer une class Personne qui possèdera le nom et prénom
+
+    // 2.2 créer une liste dans laquelle tu ajouteras une nouvelle personne à chaque fois que quelqu'un clic sur le bouton<br>
+        // /!\ ne créer pas une personne si les 2 champs sont vides-
+        
+
+
+})()
